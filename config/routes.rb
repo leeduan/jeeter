2e33ce8d5a1b76bcef0d600e7a630edd09152da4
Lee Duan <lee.duan1@gmail.com>
2014-04-27 14:09:42 -0700

    pages#front_page root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Railspress::Application.routes.draw do
-
+  root to: 'pages#front_page'
+  get '/front', to: 'pages#front_page'
 end
---
+++ spec/controllers/pages_controllers_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+describe PagesController do
+  describe 'GET front_page' do
+    it 'renders the front page template' do
+      get :front_page
+      expect(response).to render_template :front_page
+    end
+  end
+end
