83364f5e78449d40668309ce8cb83f03698fdafc
Lee Duan <lee.duan1@gmail.com>
2014-04-28 21:43:06 -0700

    create set_recent_posts and set_categories methods for sidebar instvars

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,5 +1,17 @@
 class BlogPostsController < ApplicationController
+  before_action :set_recent_posts, :set_categories
+
   def index
     @posts = PostType.find_by(name: 'Blog').posts
+  end
+
+  private
+
+  def set_recent_posts
+    @recent_posts = PostType.find_by(name: 'Blog').posts.limit(5)
+  end
+
+  def set_categories
+    @categories = Category.limit(10).order(name: :asc)
   end
 end
