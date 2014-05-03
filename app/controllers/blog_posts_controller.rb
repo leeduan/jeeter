238b2d4a44e005fca22dd4e76a476a5c1a668e33
Lee Duan <lee.duan1@gmail.com>
2014-05-02 21:07:42 -0700

    refactor blog post type query to blog_post_type private method

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -2,16 +2,20 @@ class BlogPostsController < ApplicationController
   before_action :set_recent_posts, :set_categories
 
   def index
-    @posts = PostType.find_by(name: 'Blog').posts
+    @posts = blog_post_type.posts
   end
 
   private
 
   def set_recent_posts
-    @recent_posts = PostType.find_by(name: 'Blog').posts.limit(5)
+    @recent_posts = blog_post_type.posts.limit(5)
   end
 
   def set_categories
     @categories = Category.limit(10).order(name: :asc)
+  end
+
+  def blog_post_type
+    @blog_post_type ||= PostType.find_or_create_by(name: 'Blog')
   end
 end
