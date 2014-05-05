6ab8d1018ca0af780044918fe2fb14177ebcb911
Lee Duan <lee.duan1@gmail.com>
2014-05-04 20:51:57 -0700

    user schema, admin login, cookie session

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,11 @@ Railspress::Application.routes.draw do
   root to: 'pages', action: :front_page
   get '/front', to: 'pages', action: :front_page
   get '/blog', to: 'blog_posts', action: :index
+
+  namespace :admin do
+    get '/login', to: 'sessions#index'
+    post '/login', to: 'sessions#create'
+
+    get '/', to: 'dashboard#index'
+  end
 end
