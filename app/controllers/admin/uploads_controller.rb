27ef41e477d6b0d8fb00d8076b4cf1ff9718667f
Lee Duan <lee.duan1@gmail.com>
2014-08-10 17:24:00 -0700

    minor xhr format.html refactor to uploads_controller

--- app/controllers/admin/uploads_controller.rb
+++ app/controllers/admin/uploads_controller.rb
@@ -17,12 +17,12 @@ class Admin::UploadsController < AdminController
         format.json { render json: upload, status: :created }
         format.html { redirect_to new_admin_upload_path, flash: { success: 'New file uploaded.' } }
       else
-        unless request.xhr?
+        format.json { render json: upload.errors, status: :bad_request }
+        format.html do
           @upload = upload
           flash.now[:danger] = "Error, #{@upload.errors.full_messages[0]}."
+          render :new
         end
-        format.json { render json: upload.errors, status: :bad_request }
-        format.html { render :new }
       end
     end
   end
